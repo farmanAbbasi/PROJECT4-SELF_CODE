f0dfac776d6414b4f3f946f5e17d95ad14483501
Mohammad Farman Abbasi <32016615+farmanAbbasi@users.noreply.github.com>
2024-01-22 18:32:50 +0530

    Update gitservice.service.ts

--- src/app/gitservice.service.ts
+++ src/app/gitservice.service.ts
@@ -34,6 +34,7 @@ export class GitserviceService {
       clientId: data.clientId,
       clientSecret : data.clientSecret
     }
+    console.log(d);
       return this.http.post(this.corsBASEURL+this.BASE_URL_COMPILER+"v1/execute",d).toPromise();
     }
 
